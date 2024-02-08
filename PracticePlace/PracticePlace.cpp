84e4f82286cb26a3fca4bdffdda05299861c05b2
MkGodh <fenryrvla@yandex.ru>
2024-02-08 16:09:28 +0300

    RefreshProg

--- PracticePlace/PracticePlace.cpp
+++ PracticePlace/PracticePlace.cpp
@@ -1,29 +1,44 @@
 ﻿#include <iostream>
 
+
 int main()
 {
     setlocale(LC_ALL, "RUS");
+   
+
+    int mansCount;
+    int barbersCount;
+    int manPerBarber = 8;
+    int mansPerBarberPermonth = manPerBarber * 30; // 240
+
+    std::cout << "Добро пожаловать в бородатый город!\n" <<"введите сколько проживает мужчин в городе: "; 
+    std::cin >> mansCount; //9000
+    std::cout << "Введите сколько работает барберов в городе: ";
+    std::cin >> barbersCount; //37
+
+    int needBarbers = mansCount / mansPerBarberPermonth; // 9000 / 240 = 37,5
+    if (needBarbers * mansPerBarberPermonth % mansCount) // 37,5 * 240 % 9000 = 1
+    {
+        needBarbers += 1;
+    }
+
+    std::cout << "Необходимое число барберов: " << needBarbers << "\n";
+    std::cout << needBarbers << " Барберов смогут сделать дельфинами " << needBarbers * mansPerBarberPermonth << " мужчин за месяц.\n";
+
+
+    if (needBarbers > barbersCount)
+    {
+        std::cout << "Нужно больше барберов и золота!!!\n";
+    }
+    else if (needBarbers == barbersCount)
+    {
+        std::cout << "Барберов ровно столько, что бы сделать дельфинарий!!!\n";
+    }
+    else
+    {
+        std::cout << "Барберов хватает!!!\n";
+    }
+    }
  
-    int mansInCity;
-    int barbers;
 
-    std::cout << "Добро пожаловать в Бородатый город!\n" << "Введите сколько работает барберов в городе: ";
-    std::cin >> barbers;
-    std::cout << "Введите сколько проживает мужчин в городе: ";
-    std::cin >> mansInCity;
-
-    int barberWorkShift = 8; 
-    int month = 30; 
-    int client = month * barberWorkShift; 
-    int totalLoad = barbers * client; 
-    
-     if (totalLoad >= mansInCity) {
-        std::cout << "В городе все будут выбриты и гладкие как дельфин!";
-    }
-    
-     if (totalLoad <= mansInCity) {
-        std::cout << "В городе все будут заросшие как йети!" "\n";
-        std::cout << "Для того чтобы все были дельфинами, нужно ещё: " << (mansInCity - totalLoad) / client << " барберов!";
-    }
-}
 
